fee63a94c6592f289fc797e5b8f83fb12d0fba7f
Shruti Pokale <64649386+svp12345@users.noreply.github.com>
2024-12-07 20:19:00 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use('/hello',(req,res)=>{
-    res.send('hellooo from the server')
-});
-
 app.listen(3000,()=>{
     console.log('server listening on port 3000')
 });
